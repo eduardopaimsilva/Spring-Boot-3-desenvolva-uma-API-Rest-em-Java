c8dddd6a98587bebc284c90ea149c3a44e123a4e
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-31 23:15:13 -0300

    Padronizando retornos da API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,22 +26,25 @@ public class MedicoController {
 
     }
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort ={"nome"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-
+    public ResponseEntity <Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort ={"nome"}) Pageable paginacao){
+       var page =repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+       return ResponseEntity.ok(page);
     }
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMadico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMadico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atuaLizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 
     }
     @DeleteMapping("/{id}")
     @Transactional
-    public void excuir(@PathVariable Long id){
+    public ResponseEntity excuir(@PathVariable Long id){
         var medico =repository.getReferenceById(id);
         medico.excuir();
+        return ResponseEntity.noContent().build();
 
     }
 
